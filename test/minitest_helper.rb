f73f6e820206bbb51673fa29cf16f9c5cd659e6f
Karl Smith <karl@Karls-iMac.local>
2015-04-13 20:07:46 -0700

    start some tasty tests

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mosfet'
+require 'timecop'
 
 require 'minitest/autorun'
--- test/test_mosfet.rb
+++ test/test_mosfet.rb
@@ -1,11 +1,31 @@
 require 'minitest_helper'
 
-class TestMosfet < Minitest::Test
+class TestMosfet < Minitest::Spec
+  before do
+    Timecop.freeze DateTime.new(2001, 2, 2, 0, 0, 0)
+  end
+
+  after do
+    Timecop.return
+  end
+
+
+  let(:start_time) { DateTime.new(2001, 2, 2, 0, 0, 0) }
+  let(:end_time) { DateTime.new(2001, 2, 2, 1, 0, 0) }
+
+
   def test_that_it_has_a_version_number
     refute_nil ::Mosfet::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+
+  it 'displays at date and time' do
+    assert_equal Mosfet::TimeDisplay.new(start_time).to_s, 'Fri, February 2, 12am'
   end
+
+  it 'displays the range with only time when stat and end are on same day' do
+    assert_equal Mosfet::TimeDisplay.new(start_time, end_time).to_s, 'Fri, February 2, 12am — 1am'
+  end
+
+
 end
